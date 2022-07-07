f3c993c18dcdc52eb796bfb8e8f0b0630451cb78
sadakchap <aliceprerna@gmail.com>
2022-07-07 19:33:01 +0530

    test changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="">
+      in homolog <br/>
       <div className="">NODE_ENV={process.env.NODE_ENV}</div>
       <div className="">NEXT_PUBLIC_ENV_FILE={process.env.NEXT_PUBLIC_ENV_FILE}</div>
     </div>
